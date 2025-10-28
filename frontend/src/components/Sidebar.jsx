d14d4f3c409b0fd8262fbd82618058cbcba3e8ac
priyanshiagarwall <priyanshiagarwalxc28@gmail.com>
2025-10-28 09:33:10 +0530

    Second commit

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -29,15 +29,7 @@ const Sidebar = () => {
           <span>Home</span>
         </Link>
 
-        <Link
-          to="/friends"
-          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
-            currentPath === "/friends" ? "btn-active" : ""
-          }`}
-        >
-          <UsersIcon className="size-5 text-base-content opacity-70" />
-          <span>Friends</span>
-        </Link>
+        
 
         <Link
           to="/notifications"
